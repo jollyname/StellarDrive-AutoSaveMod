1c3fe9a2eb9827881e03ae04f53c206bd9c16be5
jollyname <95495467+jollyname@users.noreply.github.com>
2026-07-19 15:16:02 +0200

    Added source code

---
+++ Main.cs
@@ -0,0 +1,48 @@
+﻿using MelonLoader;
+using Core.Services;
+using Saving.Interface.Services;
+using UnityEngine;
+
+namespace AutoSaveMod
+{
+    public static class BuildInfo
+    {
+        public const string Name = "Jolly's AutoSave Mod";
+        public const string Description = "Autosaves every 20 minutes";
+        public const string Author = "Jollyname";
+        public const string Company = null;
+        public const string Version = "1.0.0";
+        public const string DownloadLink = null;
+    }
+
+    public class AutoSaveMod : MelonMod
+    {
+        private const float saveDelay = 1200f;
+        private float saveTimer;
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (buildIndex == 1)
+            {
+                saveTimer = saveDelay;
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            saveTimer -= Time.deltaTime;
+
+            if (saveTimer <= 0f)
+            {
+                var saver = ServiceLocator.GetService<IGameSaver>();
+
+                if (saver != null)
+                {
+                    saver.SaveGame();
+                }
+
+                saveTimer = saveDelay;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using MelonLoader;
+
+[assembly: AssemblyTitle(AutoSaveMod.BuildInfo.Description)]
+[assembly: AssemblyDescription(AutoSaveMod.BuildInfo.Description)]
+[assembly: AssemblyCompany(AutoSaveMod.BuildInfo.Company)]
+[assembly: AssemblyProduct(AutoSaveMod.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + AutoSaveMod.BuildInfo.Author)]
+[assembly: AssemblyTrademark(AutoSaveMod.BuildInfo.Company)]
+[assembly: AssemblyVersion(AutoSaveMod.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(AutoSaveMod.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(AutoSaveMod.AutoSaveMod), AutoSaveMod.BuildInfo.Name, AutoSaveMod.BuildInfo.Version, AutoSaveMod.BuildInfo.Author, AutoSaveMod.BuildInfo.DownloadLink)]
+[assembly: MelonColor()]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame("CuriousOwlGames", "StellarDrive")]
