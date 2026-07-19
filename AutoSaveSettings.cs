cff922e0892164017beef6b810845dfb1398ad24
jollyname <95495467+jollyname@users.noreply.github.com>
2026-07-19 16:15:22 +0200

    Added options to settings menu

---
+++ AutoSaveSettings.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UI.Common.Options;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AutoSaveMod
+{
+    public class AutoSaveSettings : MonoBehaviour
+    {
+        private static readonly int[] Delays =
+        {
+            5,
+            10,
+            20,
+            30,
+            45,
+            60
+        };
+
+        void Start()
+        {
+            // AutoSave toggle
+            Toggle _toggle = CreateToggle("AutoSaveToggle", "Autosave");
+
+            _toggle.isOn = AutoSaveMod.autosaveEnabled.Value;
+            _toggle.onValueChanged.AddListener(OnToggleChanged);
+
+            // SaveDelay dropdown
+            TMP_Dropdown _dropdown = CreateDropdown("AutoSaveDelay", "Autosave Delay");
+
+            _dropdown.ClearOptions();
+
+            _dropdown.AddOptions(new List<string>
+            {
+                "5 minutes",
+                "10 minutes",
+                "20 minutes",
+                "30 minutes",
+                "45 minutes",
+                "60 minutes"
+            });
+
+            int index = System.Array.IndexOf(Delays, AutoSaveMod.autosaveDelay.Value);
+
+            if (index < 0) index = 2;
+
+            _dropdown.value = index;
+            _dropdown.RefreshShownValue();
+
+            _dropdown.onValueChanged.AddListener(value =>
+            {
+                AutoSaveMod.autosaveDelay.Value = Delays[value];
+                AutoSaveMod.ResetTimer();
+            });
+        }
+
+        Toggle CreateToggle(string _name, string _labelContent)
+        {
+            Transform existingToggle = transform.Find(_name);
+
+            Toggle _toggle;
+            TMP_Text _label;
+
+            if (existingToggle != null)
+            {
+                _toggle = existingToggle.GetComponentInChildren<Toggle>();
+                _label = existingToggle.GetComponentInChildren<TMP_Text>();
+            }
+            else
+            {
+                Transform devTools = transform.Find("DevTools");
+
+                GameObject obj = Instantiate(devTools.gameObject, transform);
+                Destroy(obj.GetComponent<DevToolSettings>());
+
+                obj.name = _name;
+
+                _toggle = obj.GetComponentInChildren<Toggle>();
+                _label = obj.GetComponentInChildren<TMP_Text>();
+
+                _toggle.onValueChanged.RemoveAllListeners();
+            }
+
+            _label.text = _labelContent;
+
+            return _toggle;
+        }
+
+        TMP_Dropdown CreateDropdown(string _name, string _labelContent)
+        {
+            Transform existingDropdown = transform.Find(_name);
+
+            TMP_Dropdown dropdown;
+            TMP_Text label;
+
+            if (existingDropdown != null)
+            {
+                dropdown = existingDropdown.GetComponentInChildren<TMP_Dropdown>();
+                label = existingDropdown.GetComponentInChildren<TMP_Text>();
+            }
+            else
+            {
+                Transform windowMode = transform.Find("WindowMode");
+
+                GameObject obj = Instantiate(windowMode.gameObject, transform);
+                obj.name = _name;
+
+                dropdown = obj.GetComponentInChildren<TMP_Dropdown>();
+                label = obj.GetComponentInChildren<TMP_Text>();
+
+                dropdown.onValueChanged.RemoveAllListeners();
+            }
+
+            label.text = _labelContent;
+
+            return dropdown;
+        }
+
+        private void OnToggleChanged(bool value)
+        {
+            AutoSaveMod.autosaveEnabled.Value = value;
+
+            if (value) AutoSaveMod.ResetTimer();
+        }
+    }
+}
--- Main.cs
+++ Main.cs
@@ -2,46 +2,103 @@
 using Core.Services;
 using Saving.Interface.Services;
 using UnityEngine;
+using UI.Common.Options;
 
 namespace AutoSaveMod
 {
     public static class BuildInfo
     {
         public const string Name = "Jolly's AutoSave Mod";
-        public const string Description = "Autosaves every 20 minutes";
+        public const string Description = "Autosaves your game with a customizable interval";
         public const string Author = "Jollyname";
         public const string Company = null;
-        public const string Version = "1.0.0";
+        public const string Version = "1.1.0";
         public const string DownloadLink = null;
     }
 
     public class AutoSaveMod : MelonMod
     {
-        private const float saveDelay = 1200f;
-        private float saveTimer;
+        private static float saveTimer;
+
+        private static GameObject selectedMenu;
+
+        private static MelonPreferences_Category preferencesCategory;
+        public static MelonPreferences_Entry<bool> autosaveEnabled;
+        public static MelonPreferences_Entry<int> autosaveDelay;
+
+        public override void OnInitializeMelon()
+        {
+            preferencesCategory = MelonPreferences.CreateCategory("AutoSaveMod");
+
+            // AutoSave bool entry
+            if (preferencesCategory.HasEntry("autosave"))
+            {
+                autosaveEnabled = preferencesCategory.GetEntry<bool>("autosave");
+            }
+            else
+            {
+                autosaveEnabled = preferencesCategory.CreateEntry(
+                    "autosave",
+                    true
+                );
+            }
+
+            // SaveDelay int entry
+            if (preferencesCategory.HasEntry("autosaveDelay"))
+            {
+                autosaveDelay = preferencesCategory.GetEntry<int>("autosaveDelay");
+            }
+            else
+            {
+                autosaveDelay = preferencesCategory.CreateEntry(
+                    "autosaveDelay",
+                    20
+                );
+            }
+        }
+
+        public static void ResetTimer()
+        {
+            saveTimer = autosaveDelay.Value * 60f;
+        }
 
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
-            if (buildIndex == 1)
+            if (buildIndex == 0)
             {
-                saveTimer = saveDelay;
+                selectedMenu = GameObject.Find("SelectedMenu");
+            }
+            else if (buildIndex == 1)
+            {
+                selectedMenu = GameObject.Find("PauseCanvas");
+                ResetTimer();
             }
         }
 
         public override void OnUpdate()
         {
+            if (selectedMenu != null)
+            {
+                var settings = selectedMenu.GetComponentInChildren<VolumeSettings>();
+
+                if (settings != null && settings.GetComponent<AutoSaveSettings>() == null)
+                {
+                    settings.gameObject.AddComponent<AutoSaveSettings>();
+                }
+            }
+
+            if (!autosaveEnabled.Value)
+                return;
+
             saveTimer -= Time.deltaTime;
 
             if (saveTimer <= 0f)
             {
                 var saver = ServiceLocator.GetService<IGameSaver>();
 
-                if (saver != null)
-                {
-                    saver.SaveGame();
-                }
+                if (saver != null) saver.SaveGame();
 
-                saveTimer = saveDelay;
+                ResetTimer();
             }
         }
     }
